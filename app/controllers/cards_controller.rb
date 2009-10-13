35e0f6b0baf9fce3a6f5c5e5170b4b03ff0d93df
Nicholas Tommarello <nick@tommarello.net>
2009-10-13 19:21:00 -0400

    map

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -173,18 +173,15 @@ class CardsController < ApplicationController
       	pdf2.image pigs, :at => [-36,0], :fit => [414, 324]
       end
       
-      pdf.bounding_box [0,288], :width => 414 do
-      	pigs = "#{RAILS_ROOT}/public/images/cards/back.jpg" 
-      	pdf.image pigs, :at => [-36,0], :fit => [414, 324]
-      end
+  
 
 
       if @card.lat
       	if @card.lat != 0
       		s = Hash.new
       	s[:pic_google_map]="http://maps.google.com/staticmap?center=#{@card.lat},#{@card.lng}&zoom=10&size=640x456&maptype=terrain&markers=#{@card.lat},#{@card.lng},red&key=ABQIAAAALF-_g5rNREDtjEue6txM3xQP7nTGYYVCHHTU9L3Hb_ZDidErMhSGz6PAfvIlqt6bAp17W_SZPV9HeA&format=jpg-baseline"
-      		pdf.bounding_box [149,270], :width => 210 do
-      		pdf.image open(s[:pic_google_map]), :at => [0,0], :fit => [210, 150]
+      		pdf2.bounding_box [149,270], :width => 210 do
+      		pdf2.image open(s[:pic_google_map]), :at => [0,0], :fit => [210, 150]
     		end
       	end
        end
