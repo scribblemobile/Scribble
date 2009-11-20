3cf57986af5ad065ac7ea1bff5d44f7c43ea914a
Nicholas Tommarello <nick@tommarello.net>
2009-11-20 15:18:12 -0500

    asd

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -15,7 +15,7 @@ class DiscountsController < ApplicationController
     #first check if valid code
     @codes = Discount.find_by_code(params[:code])
     
-    if @codes.count > 0
+    unless @codes.nil?
       @valid = 1
     else
       @valid = 0
@@ -25,7 +25,7 @@ class DiscountsController < ApplicationController
     #then check the code has not been used by this person
     if @valid == 1
       @redeemed = Redemption.find_by_code_and_device_id(params[:code],params[:device_id])
-      if @redeemed.count > 0
+      unless @redeemed.nil?
         @valid = 0
         @error = "Code already used"
       end
